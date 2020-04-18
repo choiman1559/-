8b024e9ed24ba11c04c14bb67cbb62090fac5f69
choiman1559 <cuj1559@gmail.com>
2020-04-18 21:44:05 +0900

    fixed where AlarmManager probably isn't work

--- app/src/main/java/com/mask/uses/counter/NotificationClass.java
+++ app/src/main/java/com/mask/uses/counter/NotificationClass.java
@@ -5,9 +5,11 @@ import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.widget.RemoteViews;
 
@@ -22,7 +24,7 @@ class NotificationClass {
     static class Notification {
         static void Notify(Context context) {
             Intent BroacastIntent = new Intent(context, OnReloadClick.class);
-            BroacastIntent.setAction("com.mask.uses.counter.MASK_RELOAD").putExtra("code","666");
+            BroacastIntent.setAction("com.mask.uses.counter.MASK_RELOAD").putExtra("code", "666");
             PendingIntent BroadcastPending = PendingIntent.getBroadcast(context, 666, BroacastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
             Intent mIntent = new Intent(context, MainActivity.class);
@@ -32,8 +34,8 @@ class NotificationClass {
 
             RemoteViews contentView = new RemoteViews(context.getPackageName(), R.layout.notification);
             contentView.setTextViewText(R.id.toptext, "현재 사용중인 마스크는\n" + prefs.getInt("MaskDays", 0) + "일 째 사용중입니다!");
-            contentView.setTextViewText(R.id.textview17,context.getString(R.string.ChangeMask));
-            contentView.setOnClickPendingIntent(R.id.textview17,BroadcastPending);
+            contentView.setTextViewText(R.id.textview17, context.getString(R.string.ChangeMask));
+            contentView.setOnClickPendingIntent(R.id.textview17, BroadcastPending);
             contentView.setImageViewResource(R.id.reload_icon, R.drawable.ic_autorenew_black_24dp);
             contentView.setOnClickPendingIntent(R.id.reload_icon, BroadcastPending);
             contentView.setImageViewResource(R.id.mask_icon, R.drawable.ic_dentist_mask);
@@ -72,25 +74,30 @@ class NotificationClass {
 
         Alarm(Context context) {
             Alarm.context = context;
-            Intent BroacastIntent = new Intent(Alarm.context,DayLaterClass.class).putExtra("code","999");
+            Intent BroacastIntent = new Intent(Alarm.context, DayLaterClass.class).putExtra("code", "999");
             BroacastIntent.setAction("com.mask.uses.counter.DAY_LATER");
-            BroadcastPending = PendingIntent.getBroadcast(Alarm.context,999,BroacastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+            BroadcastPending = PendingIntent.getBroadcast(Alarm.context, 999, BroacastIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         }
 
         static void setAlarm() {
             Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(System.currentTimeMillis());
-            calendar.set(Calendar.HOUR, 0);
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE,0);
 
-            alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+            alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
             assert alarmMgr != null;
-            alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                    AlarmManager.INTERVAL_DAY, BroadcastPending);
+            alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, BroadcastPending);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                alarmMgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), BroadcastPending);
+            }
+            context.getPackageManager().setComponentEnabledSetting(
+                    new ComponentName(context, BootComplete.class),
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                    PackageManager.DONT_KILL_APP);
         }
 
         static void cancelAlarm() {
             if (alarmMgr != null) alarmMgr.cancel(BroadcastPending);
         }
-
     }
 }
